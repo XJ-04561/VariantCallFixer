956189578cef7683ca021efdafc5f5e1dbba5690
XJ-04561 <107653760+XJ-04561@users.noreply.github.com>
2024-03-08 17:09:22 +0100

    Bugfix

--- VariantCallFixer/__init__.py
+++ VariantCallFixer/__init__.py
@@ -1,8 +1,8 @@
 
 
+from VariantCallFixer.Functions import openVCF
 from VariantCallFixer.CreateVCF import CreateVCF
 from VariantCallFixer.ReadVCF import ReadVCF
 from VariantCallFixer.RowDict import RowDict
-from VariantCallFixer.Functions import openVCF
 import VariantCallFixer.Functions as Functions
 import VariantCallFixer.Globals as Globals
